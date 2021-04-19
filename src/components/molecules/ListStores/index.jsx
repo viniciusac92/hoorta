9df396487aaf7e75c2f8f45236f26974b9cad559
João Pedro Arruda <jaaopbr@gmail.com>
2021-04-19 02:47:12 -0300

    🐛HotFix: Fixed storesList issues

--- src/components/molecules/ListStores/index.jsx
+++ src/components/molecules/ListStores/index.jsx
@@ -2,8 +2,13 @@ import StoreCard from "../../atoms/StoreCard";
 import { useStores } from "../../../providers/StoresContext";
 import { List } from "./style";
 import TextHeader from "../../atoms/TextHeader";
+import { useEffect } from "react";
 const ListStores = () => {
-  const { listStores } = useStores();
+  const { listStores, getAllStores } = useStores();
+
+  useEffect(() => {
+    getAllStores();
+  }, []);
   return (
     <List>
       <div>
